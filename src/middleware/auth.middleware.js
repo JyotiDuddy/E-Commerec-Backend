53c79e1825e68fbe3cfe697300dead0f0ae64631
Jyoti Duddy <jyotiduddy00@gmail.com>
2026-08-09 18:04:14 +0530

    Update AuthMiddleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,51 +1,56 @@
-const User= require("../models/user.model");
-const jwt= require("jsonwebtoken")
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
+const authMiddleware = async (req, res, next) => {
+  console.log("========== AUTH MIDDLEWARE ==========");
 
-const authMiddleware= async(req,res,next)=>{
-  const {token} = req.cookies;
+  console.log("Cookies:", req.cookies);
 
-  if(!token){
+  const { token } = req.cookies;
+
+  if (!token) {
+    console.log("❌ NO TOKEN");
+
     return res.status(401).json({
-      message:"Unauthorized",
-    })
+      message: "Unauthorized",
+    });
   }
 
-  try{
-      const decoded=jwt.verify(token,process.env.JWT_SECRET)
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  if(!decoded){
+    console.log("Decoded JWT:", decoded);
+
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Invalid Token",
+      });
+    }
+
+    const user = await User.findById(decoded.id);
+
+    console.log("User from database:", user);
+    console.log("User isAdmin:", user?.isAdmin);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+    req.userId = user._id;
+
+    next();
+
+  } catch (err) {
+    console.log("AUTH ERROR:", err);
+
     return res.status(401).json({
-      message:"Invalid Token"
-    })
+      success: false,
+      message: "Invalid or expired token",
+    });
   }
+};
 
-  const user = await User.findById(decoded.id);
-
-  if(!user){
-    return res.status(404).json({
-      message:"User not found "
-    })
-  }
-
-  req.user = user;
-  req.userId = user._id;
-
-
-  next()
-
-  }catch(err){
-  console.log(err);
-    return res.status(401).json({
-    success: false,
-    message: "Invalid or expired token",
-  });
-
-  }
-
-
-
-
-}
-
-module.exports = {authMiddleware}
+module.exports = { authMiddleware };
